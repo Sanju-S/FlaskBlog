1ce98fde8e381f651b2118d38a60db2fb107ac0a
sanjsark <sarkarsanju97@gmail.com>
2021-08-13 18:54:44 +0530

    Can create posts

--- website/auth.py
+++ website/auth.py
@@ -24,7 +24,7 @@ def login():
         else:
             flash('Email does not exist!', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/sign-up", methods=['GET', 'POST'])
@@ -55,7 +55,7 @@ def sign_up():
             flash('User Created')
             return redirect(url_for('views.home'))
 
-    return render_template("sign-up.html")
+    return render_template("sign-up.html", user=current_user)
 
 
 @auth.route("/logout")
